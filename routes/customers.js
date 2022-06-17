eca53d28cbee893bb4b6cda2e95736c3a0a55ff1
roanvonb <roanvonb@gmail.com>
2022-06-17 11:57:21 +1000

    put requests for customers and genres

--- routes/customers.js
+++ routes/customers.js
@@ -23,4 +23,20 @@ router.post('/', async (req, res) => {
   res.send(customer);
 });
 
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone
+    }, { new: true });
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+  
+  res.send(customer);
+});
+
 module.exports = router; 
--- routes/genres.js
+++ routes/genres.js
@@ -18,4 +18,17 @@ router.post('/', async (req, res) => {
     res.send(genre);
 });
 
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+      new: true
+    });
+  
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    
+    res.send(genre);
+});
+
 module.exports = router; 
