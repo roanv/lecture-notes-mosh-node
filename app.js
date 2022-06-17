a0cb8eba40a428f37362498092a7754429dfd4c7
roanvonb <roanvonb@gmail.com>
2022-06-17 12:47:47 +1000

    installed and enabled pug for an index.html page

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const mongoose = require('mongoose'); 
 const startupDebugger = require('debug')('app:startup');
-const dbDebugger = require('debug')('app:db');
-const config = require('config');
+const dbDebugger = require('debug')('app:db'); // second call is arbitrary namespace
+const config = require('config'); 
 const logger = require('./middleware/logger')  // custom middleware
-const helmet = require('helmet'); 
-const morgan = require('morgan'); 
-const auth = require('./auth')
+const helmet = require('helmet'); // header protection
+const morgan = require('morgan');  // logging http requests
 
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'pug'); // enable pug
+app.set('views','./views'); // optional (this is default value)
 
 // DB CONNECT
 mongoose.connect('mongodb://localhost/vidly')  // db created the first time something is written to it
@@ -27,15 +29,15 @@ app.use(express.json()) // parsing json - creates json 'body' object on req
 app.use(express.static('public')); // serve static pages in public folder
 app.use(logger);
 app.use(helmet()); // header security
-app.use(auth);
 
 // console.log(process.env.NODE_ENV);
 // app.get('env') similar to process.env.NODE_ENV; returns development by default
 if (app.get('env') === 'development'){  
     app.use(morgan('tiny')); // HTTP request logger 
     //console.log('morgan enabled...');
-    startupDebugger('morgan enabled...'); // looks at process.env.DEBUG = app:startup
-    dbDebugger('connected to db');  // looks at process.env.DEBUG = app:db OR DEBUG=app:*
+    startupDebugger('morgan enabled...'); // looks at process.env.DEBUG == app:startup
+    dbDebugger('connected to db');  // looks at process.env.DEBUG == app:db
+    // can also set env variable to DEBUG=app:* to log all in app namespace
 }
 
 // CONFIGURATION
@@ -47,7 +49,7 @@ console.log('Mail Server: ' + config.get('mail.host'));
 // sets variable for that powershell session
 
 // ROUTES
-app.get('/',(req, res) => {res.send('Vidly Home');})
+app.get('/',(req, res) => {res.render('index',{title:'Vidly',message:'Welcome to Vidly!'});}) // pug render
 app.use('/api/customers',customers);
 app.use('/api/genres',genres);
 
