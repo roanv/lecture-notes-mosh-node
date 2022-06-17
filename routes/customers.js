55d54edc6ab6c0e0f677e15d3212f05f6324319b
roanvonb <roanvonb@gmail.com>
2022-06-17 12:00:42 +1000

    get requests for individual customers and genres

--- routes/customers.js
+++ routes/customers.js
@@ -47,4 +47,12 @@ router.delete('/:id', async (req, res) => {
   res.send(customer);
 });
 
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(customer);
+});
+
 module.exports = router; 
--- routes/genres.js
+++ routes/genres.js
@@ -38,5 +38,13 @@ router.delete('/:id', async (req, res) => {
   
     res.send(customer);
 });
+
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
   
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
+    res.send(genre);
+});
+
 module.exports = router; 
