2846a344fd322833830c4d350150145025bb5ffd
roanvonb <roanvonb@gmail.com>
2022-06-17 10:52:37 +1000

    added mongoose and connected to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose'); 
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')  // db created the first time something is written to it
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.get('/', (req,res) => {
     res.send('test')
