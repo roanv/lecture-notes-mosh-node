928f39807e7fc51eeabd8165e79985b8ec82c411
roanvonb <roanvonb@gmail.com>
2022-06-17 12:08:28 +1000

    added db import notes

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 mongoose.connect('mongodb://localhost/vidly')  // db created the first time something is written to it
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
+  
+//  mongo import from json cmd
+//  mongoimport --db database-name --collection collection-name --file C:/path/to/data.json --jsonArray
 
 // ROUTES
 app.use(express.json())
