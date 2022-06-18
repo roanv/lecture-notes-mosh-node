6fc1a9a28eb390afddee44fcf37f53c6ce1d0b13
roanvonb <roanvonb@gmail.com>
2022-06-18 17:43:29 +1000

    fixed bug in movie routes where genre was not looked up

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const {Movie, validate} = require('../models/movie');
+const { Genre } = require('../models/genre');
 
 router.get('/', async (req, res) => {
     const movies = await Movie.find().sort('title');
@@ -11,8 +12,19 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
-  
-    let movie = new Movie({ title: req.body.title });
+    
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid Genre.');
+
+    let movie = new Movie({ 
+        title: req.body.title,
+        genre:{
+            id: genre.id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+     });
     movie = await movie.save();
     
     res.send(movie);
@@ -21,8 +33,19 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid Genre.');
   
-    const movie = await Movie.findByIdAndUpdate(req.params.id, { title: req.body.name }, {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, { 
+        title: req.body.title,
+        genre:{
+            id: genre.id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+     }, {
       new: true
     });
   
