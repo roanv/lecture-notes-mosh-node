82f03d563a7d1a7aad32b7c59f588a6ce5de29d7
roanvonb <roanvonb@gmail.com>
2022-06-18 10:15:08 +1000

    fixed bug where customer was queried instead of genre

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    }
-  }));
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+})
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
     const schema = Joi.object({
@@ -20,3 +22,4 @@ function validateGenre(genre) {
 
 exports.validate = validateGenre;
 exports.Genre = Genre; 
+exports.genreSchema = genreSchema;
