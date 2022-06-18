e8400eeb8208520c0c1bfdf767049ba4057bf9ca
roanvonb <roanvonb@gmail.com>
2022-06-18 10:16:18 +1000

    model for movies

---
+++ models/movie.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const {genreSchema} = require('./genre');
+
+const Movie = mongoose.model('Movie', new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true, 
+        minlength: 5,
+        maxlength: 255
+    },
+    genre: { 
+        type: genreSchema,  
+        required: true
+      },
+      numberInStock: { 
+        type: Number, 
+        required: true,
+        min: 0,
+        max: 255
+      },
+      dailyRentalRate: { 
+        type: Number, 
+        required: true,
+        min: 0,
+        max: 255
+      }
+}));
+
+function validateMovie(movie) {
+    const schema = Joi.object({
+        title: Joi.string().min(5).max(50).required(),
+        genreId: Joi.string().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: Joi.number().min(0).required()
+    });
+    return schema.validate(movie);
+}
+
+exports.Movie = Movie; 
+exports.validate = validateMovie;
