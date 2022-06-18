ce5f09772703888940087c193209dcf6c2019563
roanvonb <roanvonb@gmail.com>
2022-06-18 10:18:35 +1000

    formatting

--- models/movie.js
+++ models/movie.js
@@ -3,29 +3,29 @@ const Joi = require('joi');
 const {genreSchema} = require('./genre');
 
 const Movie = mongoose.model('Movie', new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true, 
-        minlength: 5,
-        maxlength: 255
-    },
-    genre: { 
-        type: genreSchema,  
-        required: true
-      },
-      numberInStock: { 
-        type: Number, 
-        required: true,
-        min: 0,
-        max: 255
-      },
-      dailyRentalRate: { 
-        type: Number, 
-        required: true,
-        min: 0,
-        max: 255
-      }
+  title: {
+    type: String,
+    required: true,
+    trim: true, 
+    minlength: 5,
+    maxlength: 255
+  },
+  genre: { 
+    type: genreSchema,  
+    required: true
+  },
+  numberInStock: { 
+    type: Number, 
+    required: true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate: { 
+    type: Number, 
+    required: true,
+    min: 0,
+    max: 255
+  }
 }));
 
 function validateMovie(movie) {
