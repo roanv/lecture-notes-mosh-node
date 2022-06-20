7fe0e50193b05b7aa4a05428f633c089465d779e
roanvonb <roanvonb@gmail.com>
2022-06-20 14:48:28 +1000

    formatting

--- models/user.js
+++ models/user.js
@@ -3,37 +3,37 @@ const Joi = require('joi');
 const e = require('express');
 
 const userSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
       type: String,
       required: true,
       minlength: 5,
-      maxlength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique:true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1024
-      }
-  })
+      maxlength: 255,
+      unique:true
+  },
+  password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024
+    }
+})
 
-  const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-  function validateUser(user) {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(1024).required()
-    });
+function validateUser(user) {
+  const schema = Joi.object({
+      name: Joi.string().min(5).max(50).required(),
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(1024).required()
+  });
 
-    return schema.validate(user);
+  return schema.validate(user);
 }
 
 exports.User = User;
