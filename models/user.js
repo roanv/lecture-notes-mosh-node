15335fa7fa07788129088698aad334e55e12849d
roanvonb <roanvonb@gmail.com>
2022-06-20 11:20:32 +1000

    added user model

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Joi = require('joi');
+const e = require('express');
+
+const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+      }
+  })
+
+  const User = mongoose.model('User', userSchema);
+
+  function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required()
+    });
+
+    return schema.validate(user);
+}
+
+exports.User = user;
+exports.validate = validateUser;
