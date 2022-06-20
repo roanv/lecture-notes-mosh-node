39de416a05c15cef0e8ef07f6d69caa8b96fa480
roanvonb <roanvonb@gmail.com>
2022-06-20 14:52:22 +1000

    added auth route to handle user authentication

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const {User} = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+
+function validate(req) {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required()
+    });
+
+    return schema.validate(user);
+}
+// register
+router.post('/', async (req, res) => {
+
+    // check valid input
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // check user registered
+    let user = await User.findOne({email:req.body.email});
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+    res.send(true);
+});
+
+module.exports = router;
