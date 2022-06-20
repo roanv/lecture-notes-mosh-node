de743eb36da666eafd0ef5359eb9f9d812d99ce7
roanvonb <roanvonb@gmail.com>
2022-06-20 14:20:24 +1000

    fixed user registration POST - missing await

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,13 @@ const {User, validate} = require('../models/user');
 
 // register
 router.post('/', async (req, res) => {
+
+    // check valid input
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = User.findOne({email:req.body.email});
+    // check user already registered
+    let user = await User.findOne({email:req.body.email});
     if (user) return res.status(400).send('User already registered');
 
     user = new User({
