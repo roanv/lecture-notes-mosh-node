6c6a09fee1eaf9d14cf0150d5ff350f02b7ee09b
roanvonb <roanvonb@gmail.com>
2022-06-20 16:28:31 +1000

    user get req in users route by their jwtoken

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,14 @@ const mongoose = require('mongoose');
 const {User, validate} = require('../models/user');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
+
+router.get('/me', auth, async (req, res) => { 
+    const user = await User
+    .findById(req.user.id)// look up user by their decoded jwt (by auth middleware)
+    .select('-password'); // exclude sending password
+    res.send(user);
+});
 
 // register
 router.post('/', async (req, res) => {
