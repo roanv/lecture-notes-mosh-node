9846a197aea9e24fcf0e1319e91afdb481aee471
roanvonb <roanvonb@gmail.com>
2022-06-20 14:41:33 +1000

    implemented hashed passwords for users

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const {User, validate} = require('../models/user');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 // register
 router.post('/', async (req, res) => {
@@ -15,12 +16,18 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email:req.body.email});
     if (user) return res.status(400).send('User already registered');
 
+    // create user
     // user = new User({
     //     name:req.body.name,
     //     email:req.body.email,
     //     password:req.body.password
-    // });
-    user = new User(_.pick(req.body,['name','email','password'])); // same result as above
+    // }); // instead of this, shorthand with lodash pick is:
+
+    user = new User(_.pick(req.body,['name','email','password']));
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password,salt);
 
     await user.save();
 
