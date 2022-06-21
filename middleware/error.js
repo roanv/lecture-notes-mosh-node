e1fcf4bd35febc50ccf7eb9ef728b3a36fb81af6
roanvonb <roanvonb@gmail.com>
2022-06-21 10:42:27 +1000

    better explanation of logging levels

--- middleware/error.js
+++ middleware/error.js
@@ -2,12 +2,14 @@ const winston = require('winston');
 
 module.exports = function(err,req,res,next){
     winston.error(err.message, {metadata:err});
-    // LOGGING LEVELS
+
+    // LOGGING LEVELS  // cascades up eg. if "info", everything above "info" will also be logged
     // error 
     // warn
     // info
     // verbose
     // debug
     // silly
+    
     res.status(500).send('Something went wrong.');
 }
