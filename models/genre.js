d173440806635f47675a04f7d09a3744a7096ece
roanvonb <roanvonb@gmail.com>
2022-06-21 17:01:33 +1000

    fixed Joi bug where genre length was not set correctly

--- models/genre.js
+++ models/genre.js
@@ -15,7 +15,7 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
     const schema = Joi.object({
-        name: Joi.string().min(3).required()
+        name: Joi.string().min(5).max(50).required()
     });
 
     return schema.validate(genre);
