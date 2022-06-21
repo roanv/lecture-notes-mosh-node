33097d82c96128dd2ef37a13715173dddd8e4bd5
roanvonb <roanvonb@gmail.com>
2022-06-21 16:50:57 +1000

    added integration tests for genre GET/id

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const mongoose = require('mongoose');
 let server;
 
 describe('/api/genres', () => {
@@ -24,5 +25,31 @@ describe('/api/genres', () => {
             expect(res.body.some(g => g.name === 'Horror')).toBeTruthy();
             expect(res.body.some(g => g.name === 'Comedy')).toBeTruthy();
         })
+    });
+
+    describe('GET /:id', () => {
+        it('should return a genre if valid id is passed', async () => {
+          const genre = new Genre({ name: 'Horror' });
+          await genre.save();
+    
+          const res = await request(server).get('/api/genres/' + genre.id);
+    
+          expect(res.status).toBe(200);
+          expect(res.body).toHaveProperty('name', genre.name);     
+        });
+    
+        it('should return 404 if invalid id is passed', async () => {
+          const id = new mongoose.Types.ObjectId();
+          const res = await request(server).get('/api/genres/' + id);
+    
+          expect(res.status).toBe(404);
+        });
+    
+        it('should return 404 if no genre with the given id exists', async () => {
+          const id = new mongoose.Types.ObjectId();
+          const res = await request(server).get('/api/genres/' + id);
+    
+          expect(res.status).toBe(404);
+        });
     });
 });
