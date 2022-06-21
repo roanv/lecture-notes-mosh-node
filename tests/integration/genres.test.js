7fd85292490b9f0dc935cf32223a6f067ca0d289
roanvonb <roanvonb@gmail.com>
2022-06-21 17:02:11 +1000

    implemenmted integration tests for genre POST

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const {User} = require('../../models/user');
 const mongoose = require('mongoose');
 let server;
 
@@ -52,4 +53,59 @@ describe('/api/genres', () => {
           expect(res.status).toBe(404);
         });
     });
+
+    describe('POST /', () => {
+        let token; 
+        let name; 
+    
+        const exec = async () => {
+          return await request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({ name });
+        }
+    
+        beforeEach(() => {
+          token = new User().generateAuthToken();      
+          name = 'Adventure'; 
+        })
+    
+        it('should return 401 if client is not logged in', async () => {
+          token = ''; 
+    
+          const res = await exec();
+    
+          expect(res.status).toBe(401);
+        });
+    
+        it('should return 400 if genre is less than 5 characters', async () => {
+          name = '1234'; 
+          
+          const res = await exec();
+    
+          expect(res.status).toBe(400);
+        });
+    
+        it('should return 400 if genre is more than 50 characters', async () => {
+          name = new Array(52).join('a');
+    
+          const res = await exec();
+    
+          expect(res.status).toBe(400);
+        });
+    
+        it('should save the genre if it is valid', async () => {
+          await exec();
+    
+          const genre = await Genre.find({ name });
+    
+          expect(genre).not.toBeNull();
+        });
+    
+        it('should return the genre if it is valid', async () => {
+          const res = await exec();
+    
+          expect(res.body.name).toBe(name);
+        });
+      });
 });
