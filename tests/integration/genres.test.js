c5f191e6e76dbcf3bdd107de3d5244cc2c9060d0
roanvonb <roanvonb@gmail.com>
2022-06-21 16:03:52 +1000

    updated genre GET/ integration test to be more strict

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,14 +1,28 @@
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
-    beforeEach(()=>{server = require('../../app')}); // need to load server before and close after each test 
-    afterEach(()=>{server.close()});
+    beforeEach(async ()=>{ 
+        server = require('../../app') // need to load server before and close after each test 
+        await Genre.remove({});
+    }); 
+    afterEach(async ()=>{
+        server.close()
+        await Genre.remove({});
+    });
 
     describe('GET /', () => {
         it('should return all genres', async () => {
+            Genre.collection.insertMany([
+                {name:'Horror'},
+                {name:'Comedy'},
+            ]);
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'Horror')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'Comedy')).toBeTruthy();
         })
     });
 });
