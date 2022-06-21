73d96d40693334e759d37cff28910c2a422d1d0b
roanvonb <roanvonb@gmail.com>
2022-06-21 15:03:16 +1000

    separated test and production db

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function(){
-    mongoose.connect('mongodb://localhost/vidly')  // db created the first time something is written to it
-    .then(() => winston.info('Connected to MongoDB...'));
+    const dbPath = config.get('db');
+    mongoose.connect(dbPath)  // db created the first time something is written to it
+    .then(() => winston.info(`Connected to ${dbPath}...`));
 }
