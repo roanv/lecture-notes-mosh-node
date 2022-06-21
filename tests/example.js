a32c2eb3a4c416bbcbd29fe49ef9e3126f8217ed
roanvonb <roanvonb@gmail.com>
2022-06-21 14:11:12 +1000

    isolating unit tests that have external dependencies

--- tests/example.js
+++ tests/example.js
@@ -1,3 +1,18 @@
 module.exports.absolute = function (number){
     return (number >= 0) ? number : -number;
 }
+
+module.exports.applyDiscount = function (order) {
+    const customer = db.getCustomer(order.customerId);
+    if (customer.points > 10) order.totalPrice *= 0.9;
+}
+
+// this is the "real" external dependency db function that would normally talk to the db
+db = function () { 
+    getCustomer = function (id){
+        console.log('Getting customer from database...');
+        return {id: 12345, points:11};
+    }
+}
+
+module.exports.db = db;
