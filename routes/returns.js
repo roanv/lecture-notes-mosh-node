5a931b2b3a30f71e87b7de702ae43b6b35bfda8d
roanvonb <roanvonb@gmail.com>
2022-06-22 17:51:48 +1000

    refactored returns route and used joi for validation

--- routes/returns.js
+++ routes/returns.js
@@ -4,29 +4,41 @@ const {Rental} = require('../models/rental');
 const {Movie} = require('../models/movie');
 const auth = require('../middleware/auth');
 const moment = require('moment');
+const Joi = require('joi');
 
 router.post('/',auth, async (req,res) => {
-    const customerId = req.body.customerId;
-    const movieId = req.body.movieId;
+    const { error } = validateReturn(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
 
-    if (!customerId) return res.status(400).send('Customer ID not provided.');
-    if (!movieId) return res.status(400).send('Movie ID not provided.');
+    const rental = await Rental.findOne({
+        customerId:req.body.customerId,
+        movieId:req.body.movieId
+    });
 
-    const rental = await Rental.findOne({customerId,movieId});
     if(!rental) return res.status(404).send('Rental not found.');
-
     if (rental.dateReturned) return res.status(400).send('Rental already processed');
 
     rental.dateReturned = new Date();
-    const days = moment.duration(rental.dateReturned - rental.dateOut, 'milliseconds').days();
-    rental.rentalFee = rental.movie.dailyRentalRate * days;
-    await rental.save();
 
-    const movie = await Movie.findOne({id:movieId});
+    const days = moment.duration(rental.dateReturned - rental.dateOut, 'milliseconds').days();
+    rental.rentalFee = rental.movie.dailyRentalRate * days;    
+
+    const movie = await Movie.findOne({id:rental.movie.movieId});
     movie.numberInStock += 1;
-    movie.save();
+
+    await rental.save();
+    await movie.save();
     
     res.status(200).send(rental);
 });
+
+function validateReturn(rental) {
+    const schema = Joi.object({
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required()
+    });
+    return schema.validate(rental);
+}
+
 
 module.exports = router; 
